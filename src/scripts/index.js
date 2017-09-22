a735b9fdb621431bcee6deef295b188165e06c39
IsaevAlexandr <aisaev188@gmail.com>
2017-09-22 11:54:33 +0300

    22.09.17 fixed some bugs

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,18 @@
 import {message} from './common/some'
 
 console.log(message);
+
+
+let humbBtn = document.querySelector('.hamburger__icon');
+if (humbBtn) {
+    let closeBtn = document.querySelector('.close-btn');
+    let fixedMenu = document.querySelector('.fixed-menu');
+    humbBtn.addEventListener('click',function () {
+        fixedMenu.classList.toggle("fixed-menu_show")
+        humbBtn.style.visibility = 'hidden';
+    })  
+    closeBtn.addEventListener('click',function () {
+        fixedMenu.classList.toggle("fixed-menu_show")
+        humbBtn.style.visibility = 'visible';
+    }) 
+}
