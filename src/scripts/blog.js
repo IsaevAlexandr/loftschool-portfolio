af1a98a53cd893da436a5f3811a7a9944a5b5927
IsaevAlexandr <aisaev188@gmail.com>
2017-09-25 12:34:38 +0300

    25.09.17 some js file changes

--- src/scripts/blog.js
+++ src/scripts/blog.js
@@ -1,7 +1,1 @@
-let blogMenu = document.querySelector('.blog__sidebar');
-if (blogMenu) {
-    blogMenu.addEventListener('click',function () {
-        blogMenu.classList.toggle("blog__sidebar_active")
-    })    
-}
-
+console.log('blog page')
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,31 @@
-const webpack = require('webpack'); 
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const webpack           = require('webpack'); 
+const UglifyJSPlugin    = require('uglifyjs-webpack-plugin');
+const path              = require('path');
+
+const PATH = {
+  source: {
+    about: path.join(__dirname, 'src/scripts/about.js'),
+    index: path.join(__dirname, 'src/scripts/index.js'),
+    'my-works': path.join(__dirname, 'src/scripts/my-works.js'),
+    blog: path.join(__dirname, 'src/scripts/blog.js'),
+  },
+  build: path.join(__dirname, 'src/scripts')
+};
+
 
 const config = {
-
+  entry: PATH.source,
   output: {
     filename: 'main.min.js'
   },
+  output: {
+    path: PATH.build,
+    filename: '[name].min.js',
+},
   plugins: [
-    // new UglifyJSPlugin({
-    //   sourceMap: true
-    // })
+    new UglifyJSPlugin({
+      sourceMap: true
+    })
   ]
 };
 
